0361f93493b53ad6103b33e7460715b26f583cdb
twestrick <tom@discipulus.app>
2025-11-12 17:02:16 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'capacitor-youtube-video-error-ios',
+  webDir: 'dist'
+};
+
+export default config;
